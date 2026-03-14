18cbaa218ce9dc91232fe484e972dda3e2a46174
DigitalDetective47 <ninji2701@gmail.com>
2026-03-14 09:33:55 -0400

    add app name to urls.py

--- stages/urls.py
+++ stages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import download, item
 
+app_name = "stages"
 urlpatterns = [
     path("<int:pk>", item, name="item"),
     path("<int:pk>/download", download, name="download"),
