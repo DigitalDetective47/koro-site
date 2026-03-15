c33853d3b4d515269130b710a52261e1c33cddc3
DigitalDetective47 <ninji2701@gmail.com>
2026-03-14 21:38:53 -0400

    add page for submitting new levels

--- stages/urls.py
+++ stages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import download, item
+from .views import download, item, new
 
 app_name = "stages"
 urlpatterns = [
     path("<int:pk>", item, name="item"),
     path("<int:pk>/download", download, name="download"),
+    path("new", new, name="new"),
 ]
--- stages/views.py
+++ stages/views.py
@@ -1,10 +1,13 @@
-from typing import Final
+from typing import Final, Optional
 
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.files.uploadedfile import UploadedFile
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.utils.text import slugify
-from koro import BinSlot
+from koro import BinSlot, XmlSlot
 
+from .forms import SubmissionForm
 from .models import Submission
 
 
@@ -25,3 +28,33 @@ def download(request: HttpRequest, pk: int) -> HttpResponse:
             "Content-Disposition": f'attachment; filename="{slugify(target.name)}.bin"'
         },
     )
+
+
+@login_required
+def new(request: HttpRequest) -> HttpResponse:
+    form: SubmissionForm
+    if request.method == "POST":
+        form = SubmissionForm(request.POST, request.FILES)
+        if form.is_valid():
+            file: Final[Optional[UploadedFile]] = form.files.get("stage_data")
+            if file is not None:
+                untyped_content: Final[str | bytes] = file.read()
+                content: Final[bytes] = (
+                    untyped_content.encode("shift_jis", "xmlcharrefreplace")
+                    if isinstance(untyped_content, str)
+                    else bytes(untyped_content)
+                )
+                Submission(
+                    name=form.cleaned_data["name"],
+                    stage_data=(XmlSlot if content[0] else BinSlot).deserialize(
+                        content
+                    ),
+                    creator=request.user,
+                    embed=form.cleaned_data["embed"],
+                    description=form.cleaned_data["description"],
+                    music=form.cleaned_data["music"],
+                ).save()
+                return HttpResponseRedirect("/stage/1")
+    else:
+        form = SubmissionForm()
+    return render(request, "stages/new.html", {"form": form})
